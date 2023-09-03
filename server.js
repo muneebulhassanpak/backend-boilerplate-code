cddfad6bbd5f6b9d37e81960803dafe69c657f0c
muneebulhassanpak <muneebulhassan121@gmail.com>
2023-09-03 10:57:48 +0500

    Cors package added

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
-app.get("/", (req, res, next) => {
-  res.send("Hello from nodejs");
-  res.end();
-});
+app.use(cors());
+app.get("/", (req, res, next) => {});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
